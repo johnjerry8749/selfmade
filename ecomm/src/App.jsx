50613de6cf16b9fdeef82657d845ca5e8586f46f
Johnjerry <johnjerry8749@gmail.com>
2025-08-02 07:04:29 -0700

    Fix ProductCard CSS import issue for deployment

--- ecomm/src/App.jsx
+++ ecomm/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+pimport React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
